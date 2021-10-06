bd7f1585ab03aee1bd38da04887126b4fe7533cd
DiSalvio <stephendisalvio@protonmail.com>
2021-10-06 19:38:18 +0800

    acronym python challenge, and remove pycaches

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,9 @@
 def abbreviate(words):
-    pass
+    return ''.join(
+            map(lambda word: word[0], 
+                ''.join(filter(lambda char: char.isalpha() or char == ' ',
+                      list(words.replace('-', ' ')))
+                    )
+                .split()
+                )
+            ).upper()
