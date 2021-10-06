a6c87af3260f1cf66db3564947d2f5f8986ce468
DiSalvio <stephendisalvio@protonmail.com>
2021-10-06 21:09:25 +0800

    allergies solution

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,16 +1,12 @@
-allergens = ['eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats']
+ALLERGENS = ['eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats']
+
 class Allergies:
     def __init__(self, score):
-        self.score = score
-
-
-    def allergic(self, allergen):
-        return 2**allergens.index(allergen) & self.score
-
+        self.allergies = [allergy for allergy in ALLERGENS if score & 2**ALLERGENS.index(allergy)]
 
     def allergic_to(self, item):
-        return item in self.lst()
+        return item in self.allergies
 
     @property
     def lst(self):
-        return filter(lambda x: self.allergic(self, x), allergens)
+        return self.allergies
