4d81a103881ee996358f5ee5f475b5aea4f1df04
DiSalvio <stephendisalvio@protonmail.com>
2021-12-09 20:56:24 +0800

    javascript bob speaks string parsing solution

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,3 +1,20 @@
 export const hey = (message) => {
-  throw new Error('Remove this statement and implement this function');
+  let no_letters = message.match(/[a-zA-Z]+/, '') === null
+  let all_whitespace = message.replace(/\s+/g, '') === ''
+  let yelling = message === message.toUpperCase()
+  let question = message.trim().endsWith('?')
+  if (all_whitespace) {
+    return "Fine. Be that way!"
+  }
+  if (!no_letters && yelling) {
+    if (question) {
+      return "Calm down, I know what I'm doing!"
+    } else {
+      return "Whoa, chill out!"
+    }
+  } else if (question) {
+    return "Sure."
+  } else{
+    return "Whatever."
+  }
 };
