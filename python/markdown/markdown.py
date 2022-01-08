58992098f5f4a23c94de7224b44d60472e4ab3dd
DiSalvio <stephendisalvio@lee-sfaa.com>
2022-01-08 18:35:33 +0800

    changed re match to re sub for conciseness

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -1,38 +1,18 @@
 import re
 
 def parse(markdown):
-    last_line_li = False
-    lines = markdown.split('\n')
+    s = markdown
+    s = re.sub(r'(.*)__(.*)__(.*)', r'\1<strong>\2</strong>\3', markdown)
+    s = re.sub(r'(.*)_(.*)_(.*)', r'\1<em>\2</em>\3', s)
+    s = re.sub(r'^\* (.*)', r'<li>\1</li>', s, flags=re.M)
+    s = re.sub(r'(<li>.*</li>)', r'<ul>\1</ul>', s, flags=re.S)
+    lines = s.split('\n')
     res = ''
-    for i, line in enumerate(lines):
-        header_match = re.match('(#+) (.*)', line)
-        if header_match:
+    for line in lines:
+        if header_match:= re.match('^(#+) (.*)', line, flags=re.M):
             line = f'<h{len(header_match.group(1))}>{header_match.group(2)}</h{len(header_match.group(1))}>'
-        bold_match = re.match('(.*)__(.*)__(.*)', line)
-        if bold_match:
-            line = f'{bold_match.group(1)}<strong>{bold_match.group(2)}</strong>{bold_match.group(3)}'
-        italic_match = re.match('(.*)_(.*)_(.*)', line)
-        if italic_match:
-            line = f'{italic_match.group(1)}<em>{italic_match.group(2)}</em>{italic_match.group(3)}'
 
-        li_match = re.match(r'\* (.*)', line)
-
-        if not li_match and not header_match:
-            line = '<p>' + line + '</p>'
-
-        if li_match:
-            if last_line_li:
-                line = f'<li>{li_match.group(1)}</li>'
-            else:
-                line = f'<ul><li>{li_match.group(1)}</li>'
-            last_line_li = True
-
-        if len(lines) == i+1:
-            if li_match:
-                line += '</ul>'
-            elif last_line_li:
-                line = '</ul>' + line
-                last_line_li = False
-
+        line = re.sub(r'^(?!<[hlu])(.*?$)', r'<p>\1</p>', line)
         res += line
-    return res
+        
+    return res 
